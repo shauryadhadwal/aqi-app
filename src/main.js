8bcbacaf229b10d27348ac08b7ba4eacdbc44b91
shauryadhadwal@gmail.com <shauryadhadwal@gmail.com>
2021-02-14 16:44:44 +0530

    Add header and apply Roboto font family

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
+import './styles/main.css'
 // Prime Vue Components
 
 import DataTable from 'primevue/datatable'
